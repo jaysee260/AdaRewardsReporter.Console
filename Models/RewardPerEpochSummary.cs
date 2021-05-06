8450b4236aee9fe1e7a3cfe76d6fd255c92a73d3
JC Gonzalez <programmer1718@gmail.com>
2021-05-06 13:29:19 -0400

    add DisplayName attribute to RewardsPerEpochSummary props

--- Models/RewardPerEpochSummary.cs
+++ Models/RewardPerEpochSummary.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel;
+
 namespace ADARewardsReporter
 {
     public class RewardsPerEpochSummary
     {
         public int Epoch { get; set; }
+        [DisplayName("Rewards Received On")]
         public string RewardsReceivedOn { get; set; }
+        [DisplayName("Amount (ADA)")]
         public decimal Amount { get; set; }
     }
 }
